70d00f0a8d3c4d8f6cf1f5d74a2d58ffbe279ddc
Paulo Weskley de A. Ferreira <weskleyalmeida.si@gmail.com>
2026-04-11 16:00:17 -0300

    fix: adjustments on the auth.module to work correctly with new providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,20 @@ import { JwtRefreshStrategy } from '@auth/jwt-refresh.strategy'
 import { AuthService } from '@auth/services/auth.service'
 import { PrismaModule } from '@shared/services/prisma/prisma.module'
 import { JwtAuthStrategy } from '@shared/strategies/jwt-auth.strategy'
+import { PasswordService } from './services/password.service'
+import { TokenService } from './services/token.service'
 
 @Module({
   imports: [PrismaModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, JwtAuthStrategy, JwtRefreshStrategy],
+  providers: [
+    AuthService,
+    AuthRepository,
+    JwtAuthStrategy,
+    JwtRefreshStrategy,
+    PasswordService,
+    TokenService,
+  ],
   exports: [JwtAuthStrategy, JwtRefreshStrategy],
 })
 export class AuthModule {}
